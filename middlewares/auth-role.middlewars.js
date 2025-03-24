95a3b8f42cdde49caa395e793f76620f2a78b5ff
Asadbek <admin@MacBook-Pro--Asadbek.local>
2025-03-24 23:24:25 +0500

    sessions full

--- middlewares/auth-role.middlewars.js
+++ middlewares/auth-role.middlewars.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+const Session = require("../models/session");
 
-function authMiddleware(req, res, next) {
+async function authMiddleware(req, res, next) {
   try {
     const token = req.header("Authorization")?.split(" ")[1];
     if (!token) {
@@ -15,6 +16,16 @@ function authMiddleware(req, res, next) {
       });
     }
 
+    const session = await Session.findOne({
+      where: { user_id: user.id, ip: req.ip },
+    });
+
+    if (!session) {
+      return res
+        .status(401)
+        .send({ message: "No session detected please log in again" });
+    }
+
     req.user = user;
     next();
   } catch (error) {
@@ -26,23 +37,43 @@ function authMiddleware(req, res, next) {
 }
 
 function roleMiddleware(roles) {
-  return (req, res, next) => {
+  return async (req, res, next) => {
     try {
       const token = req.header("Authorization")?.split(" ")[1];
-      if (!token)
+      if (!token) {
         return res.status(401).send({ message: "Token not provided" });
+      }
 
       const user = jwt.verify(token, "apex1");
-      if (!user) return res.status(401).send({ message: "Invalid token" });
-      req.user = user;
 
+      if (user.status !== "active") {
+        return res.status(401).send({
+          message: "You did not verify your account. Please verify it.",
+        });
+      }
+
+      const session = await Session.findOne({
+        where: { user_id: user.id, ip: req.ip },
+      });
+
+      if (!session) {
+        return res
+          .status(401)
+          .send({ message: "No session detected please log in again" });
+      }
       if (roles.includes(user.role)) {
+        req.user = user;
         next();
       } else {
-        res.status(400).send({ message: "Not allowed" });
+        return res
+          .status(403)
+          .send({ message: "You are not allowed to access this route" });
       }
     } catch (error) {
-      console.log(error);
+      if (error.name === "TokenExpiredError") {
+        return res.status(401).send({ message: "Token expired" });
+      }
+      return res.status(401).send({ message: "Invalid token" });
     }
   };
 }

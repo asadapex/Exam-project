446a21d043907faedbaa6ddb21d8b3cdcfade762
asilbekali <asilbekabdugafforov7@gmail.com>
2025-03-25 16:55:40 +0500

    fileds finished

--- models/subject.js
+++ models/subject.js
@@ -2,7 +2,6 @@ const { DataTypes } = require("sequelize");
 const { db } = require("../config/db");
 
 const Subjet = db.define("Subject", {
-<<<<<<< HEAD
     id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
@@ -16,21 +15,6 @@ const Subjet = db.define("Subject", {
         type: DataTypes.STRING,
         allowNull: false,
     },
-=======
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: false,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  image: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
->>>>>>> 2d82044354062f0b8e23eb32a017a89d19267f43
 });
 
 module.exports = Subjet;

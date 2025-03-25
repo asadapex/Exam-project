e63e29f60aecfd9926a9cf2aa22ddad50ce90440
Asadbek <admin@MacBook-Pro--Asadbek.local>
2025-03-25 11:47:10 +0500

    adding cors

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ const sessionRoutes = require("./routes/sessions");
 const registrationRoutes = require("./routes/courseRegistration");
 const uploadRoutes = require("./routes/upload");
 const categoryRoutes = require("./routes/category");
+const cors = require("cors");
 const setupSwagger = require("./swagger");
 const app = express();
 connectDb();
 
 app.use(express.json());
+app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 
 app.use("/auth", authRoutes);
 app.use("/region", regionRoutes);
